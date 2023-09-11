c17af79812bb050186ee0a67a8dc93346b014f34
Rajib Pokhrel <pokhrelrajib015@gmail.com>
2023-09-11 15:17:16 +0545

    new

--- menuBar1/index.js
+++ menuBar1/index.js
@@ -2,13 +2,21 @@
 
 var bars = document.querySelector("#bars");
 
-function showData() {
+// to show the icon of bars...
+bars.addEventListener("click", function () {
     var links = document.querySelector(".links");
-    if (links.style.display === 'none') {
-        links.style.display = 'block';
-    } else {
-        links.style.display = 'none';
-    }
-}
-bars.addEventListener("click", showData);
+    // if (links.style.display === 'none') {
+    //     links.style.display = 'block';
+    // } else {
+    //     links.style.display = 'none';
+    // }
+    links.classList.toggle("one", "two");
+});
+
+var password = document.getElementById("password");
+var showPw = document.getElementById("showPw");
+
+showPw.addEventListener("click", function() {
+password.setAttribute("type", (password.getAttribute("type") === "password" ? "text" : "password"));
+});
 
